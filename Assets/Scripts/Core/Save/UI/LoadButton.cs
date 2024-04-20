067465293ff087a63828f972a5720156e12e1226
catashX <agustinuszefanya@gmail.com>
2024-04-20 13:34:01 +0700

    init

--- Assets/Scripts/Core/Save/UI/LoadButton.cs
+++ Assets/Scripts/Core/Save/UI/LoadButton.cs
@@ -16,6 +16,7 @@ namespace CC.Core.Save.UI
         [SerializeField] GameData _data;
         [SerializeField] SaveDataHandler _handler;
         [Header("Display")]
+        [SerializeField] Image _lastCaptureImage;
         [SerializeField] TextMeshProUGUI _saveDateText;
         [SerializeField] TextMeshProUGUI _dayText;
         [SerializeField] TextMeshProUGUI _timeText;
@@ -54,8 +55,33 @@ namespace CC.Core.Save.UI
             DayTimeData _model = ((JObject)_data.saveableModelSOs[0]).ToObject<DayTimeData>();
             _nullField.SetActive(false);
             _notNullObject.SetActive(true);
+            _lastCaptureImage.sprite = loadSprite();
             _dayText.text = "Day " + _model.day.ToString();
             _timeText.text = string.Format("{0:00}:{1:00}", _model.time / 60, _model.time % 60);
+        }
+
+        Sprite loadSprite()
+        {
+            string path = Application.persistentDataPath + "/GameData/SaveShot" + slot;
+            Texture2D loaded = LoadImage(path);
+            if(loaded != null) return Sprite.Create(loaded, new Rect(0.0f, 0.0f, LoadImage(path).width, LoadImage(path).height), new Vector2(0.5f, 0.5f), 100.0f);
+            else return null;
+        }
+
+        public Texture2D LoadImage(string path)
+        {
+            if (File.Exists(path))
+            {
+                byte[] bytes = File.ReadAllBytes(path);
+                Texture2D tex = new Texture2D(2, 2);
+                tex.LoadImage(bytes);
+
+                return tex;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public void loadOnClick()
