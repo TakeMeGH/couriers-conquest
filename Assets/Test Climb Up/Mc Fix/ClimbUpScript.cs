7c2da7405ec2a5a3ac7dc26bb946acbb19ca05ef
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2024-05-06 09:43:16 +0700

    test climb up

---
+++ Assets/Test Climb Up/Mc Fix/ClimbUpScript.cs
@@ -0,0 +1,42 @@
+using CC.Events;
+using UnityEngine;
+
+namespace CC
+{
+
+    public class ClimbUpScript : MonoBehaviour
+    {
+        [SerializeField] Transform _targetPos;
+        [SerializeField] VoidEventChannelSO _triggerOnMovementStateAnimationTransitionEvent;
+        Vector3 _offsetPos;
+
+        private void OnEnable()
+        {
+            _triggerOnMovementStateAnimationTransitionEvent.OnEventRaised += AddOffset;
+        }
+
+        private void OnDisable()
+        {
+            _triggerOnMovementStateAnimationTransitionEvent.OnEventRaised -= AddOffset;
+
+        }
+        void Start()
+        {
+            _offsetPos = _targetPos.position - transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void AddOffset()
+        {
+            Debug.Log("MASUK");
+            transform.position += _offsetPos;
+        }
+
+
+    }
+}
