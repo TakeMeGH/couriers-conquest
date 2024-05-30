593953aa2d24c75b8cb0c15158a4a8d0b4fa2973
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2024-05-30 11:01:33 +0700

    feat: dialogue ui and interact ui

---
+++ Assets/Scripts/DialogueSystem/NameMapperToRole.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+namespace CC.Dialogue
+{
+    public class NameMapperToRole : MonoBehaviour
+    {
+        public AYellowpaper.SerializedCollections.SerializedDictionary<string, string> NameToRole;
+        [SerializeField] TMP_Text _characterName;
+        [SerializeField] TMP_Text _roleText;
+        void OnEnable()
+        {
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Add(OnTextChanged);
+        }
+
+        void OnDisable()
+        {
+            TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(OnTextChanged);
+        }
+
+        void OnTextChanged(Object obj)
+        {
+            if (obj == _characterName)
+            {
+                string name = _characterName.text;
+                Debug.Log(name + " DEBUG " + NameToRole[name]);
+                if (NameToRole.ContainsKey(name))
+                {
+                    _roleText.text = NameToRole[name];
+                }
+                else
+                {
+                    _roleText.text = "";
+                }
+                _roleText.ForceMeshUpdate(true);
+            }
+        }
+    }
+}
